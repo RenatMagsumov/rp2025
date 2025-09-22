8ac146c874485f55f9cf38201624f7e08a7a9d7c
RenatMagsumov <renatik@tlu.ee>
2025-09-22 21:09:48 +0300

    feat(profile): rewrite profile page with MUI components

--- 2025-09-18/myapp/src/components/Profile.tsx
+++ 2025-09-18/myapp/src/components/Profile.tsx
@@ -1,3 +1,48 @@
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AlarmIcon from '@mui/icons-material/Alarm';
+
 export default function Profile() {
-    return <h2>Profile Page</h2>;
+    return (
+        <Container maxWidth="sm" sx={{ mt: 4, textAlign: "center" }}>
+            <Box
+                sx={{
+                    p: 5,
+                    border: "1px solid #0c9c4dff",
+                    borderRadius: 2,
+                    backgroundColor: "#f3a006ff",
+                }}
+            >
+                <Typography variant="h3" gutterBottom>
+                    <b>Renat Magsumov</b>
+                </Typography>
+
+                <Typography variant="h5" gutterBottom>
+                    My hobbies
+                </Typography>
+                <List>
+                    <ListItem><b>Sports</b></ListItem>
+                    <ListItem><b>Eating delicious food</b></ListItem>
+                    <ListItem><b>Sleeping</b></ListItem>
+                </List>
+
+                <Stack spacing={2} mt={2}>
+                    <TextField label="Your Email" type="email" fullWidth />
+                    <TextField
+                        label="Your Message"
+                        multiline
+                        minRows={3}
+                        fullWidth
+                    />
+                    <Button variant="contained" endIcon={<AlarmIcon />} color="info">Contact Me</Button>
+                </Stack>
+            </Box>
+        </Container>
+    );
 }
