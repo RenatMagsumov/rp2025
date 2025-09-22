748b656bff2557267c83d9a7edcf0f365727d162
RenatMagsumov <renatik@tlu.ee>
2025-09-22 20:55:22 +0300

    feat(contact): add MUI form with TextField and Button

--- 2025-09-18/myapp/src/components/Contact.tsx
+++ 2025-09-18/myapp/src/components/Contact.tsx
@@ -1,3 +1,29 @@
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from '@mui/icons-material/Send';
+
 export default function Contact() {
-    return <h2>Contact Page</h2>;
+    return (
+        <Container maxWidth="sm" sx={{ mt: 4, textAlign: "center" }}>
+            <Stack spacing={2}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Contact
+                </Typography>
+
+                <TextField label="Your Email" type="email" fullWidth />
+
+                <TextField
+                    label="Message"
+                    multiline
+                    minRows={4}
+                    fullWidth
+                />
+
+                <Button variant="contained" endIcon={<SendIcon />} color="success">Send</Button>
+            </Stack>
+        </Container>
+    );
 }
