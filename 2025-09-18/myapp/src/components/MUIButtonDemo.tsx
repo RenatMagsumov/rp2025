098f4b74c9e2a18ac37e45e2513e66db7b9c265b
RenatMagsumov <renatik@tlu.ee>
2025-09-22 12:43:37 +0300

    feat(mui): add simple MUI Button demo component

---
+++ 2025-09-18/myapp/src/components/MUIButtonDemo.tsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function MUIButtonDemo() {
+    return (
+        <div style={{ padding: 16 }}>
+            <Button variant="contained">MUI Button</Button>
+        </div>
+    );
+}
