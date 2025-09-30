c1e0510f07b3f6bcee95c1a55a57f13496a456cd
RenatMagsumov <renatik@tlu.ee>
2025-09-30 13:12:28 +0300

    feat(examples): add PropDrilling.jsx and Context.jsx demo components

---
+++ 2025-09-25/renat/frontend/src/examples/Context.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useContext } from "react";
+
+export const WeatherContext = createContext(null);
+
+const Context = () => {
+    const [weather, setWeather] = useState("🌞");
+
+    const changeWeather = () => setWeather("🌧️");
+
+    return (
+        <WeatherContext.Provider value={{ weather, changeWeather }}>
+            <Europe />
+        </WeatherContext.Provider>
+    );
+};
+
+const Europe = () => {
+    return <Estonia />;
+};
+
+const Estonia = () => {
+    return <Tallinn />;
+};
+
+const Tallinn = () => {
+    const { weather, changeWeather } = useContext(WeatherContext);
+
+    return <div onClick={changeWeather}>{weather}</div>;
+};
+
+export default Context;
---
+++ 2025-09-25/renat/frontend/src/examples/PropDrilling.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const PropDrilling = () => {
+    const [weather, setWeather] = useState("🌞");
+
+    return <Europe weather={weather} />;
+};
+
+const Europe = ({ weather }) => {
+    return <Estonia weather={weather} />;
+};
+
+const Estonia = ({ weather }) => {
+    return <Tallinn weather={weather} />;
+};
+
+const Tallinn = ({ weather }) => {
+    return <div>{weather}</div>;
+};
+
+export default PropDrilling;
