5885497dd8cf27e6449bda59d4d289297e9424db
RenatMagsumov <renatik@tlu.ee>
2025-09-30 19:03:01 +0300

    chore(auth-backend): register /auth routes

--- 2025-09-25/renat/backend/index.js
+++ 2025-09-25/renat/backend/index.js
@@ -4,12 +4,15 @@ const port = 3000
 const cors = require("cors");
 const catsRoutes = require("./routes/cats.routes");
 const todosRouter = require("./routes/todos.routes");
+const authRouter = require("./routes/auth.routes");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/cats", catsRoutes);
 app.use("/todos", todosRouter);
+app.use("/auth", authRouter);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
