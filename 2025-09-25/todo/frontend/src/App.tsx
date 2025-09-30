b7882059fbcd8b7b3ffb84a1c5a23e235efe3fe4
RenatMagsumov <renatik@tlu.ee>
2025-09-30 21:10:58 +0300

    feat(todo-frontend): standalone UI rendering (Todos + SubmitTodo)

--- 2025-09-25/todo/frontend/src/App.tsx
+++ 2025-09-25/todo/frontend/src/App.tsx
@@ -1,27 +1,13 @@
-import { useState } from "react";
 import "./App.css";
-import Cats from "../components/Cats";
 import Todos from "../components/Todos";
-import AdminTodos from "../components/AdminTodos";
-import { Button, Stack } from "@mui/material";
+import SubmitTodo from "../components/SubmitTodo";
 
 function App() {
-  const [showAdmin, setShowAdmin] = useState(false);
-
   return (
-    <div style={{ padding: 16 }}>
-      <Stack spacing={2}>
-        <Cats />
-        <Todos />
-        <Button
-          variant="outlined"
-          onClick={() => setShowAdmin(!showAdmin)}
-        >
-          {showAdmin ? "Hide Admin Todos" : "Show Admin Todos"}
-        </Button>
-        {showAdmin && <AdminTodos />}
-      </Stack>
-    </div>
+    <>
+      <Todos />
+      <SubmitTodo />
+    </>
   );
 }
 
