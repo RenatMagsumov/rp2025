d69ed0efdce93f924ea507cca3816824c3f0e00b
RenatMagsumov <renatik@tlu.ee>
2025-09-30 18:55:06 +0300

    feat(app): initial App.tsx with Cats, Todos and AdminTodos

--- 2025-09-25/renat/frontend/src/App.tsx
+++ 2025-09-25/renat/frontend/src/App.tsx
@@ -1,8 +1,16 @@
-import './App.css'
-import Cats from "../components/Cats"
+import "./App.css";
+import Cats from "../components/Cats";
+import Todos from "../components/Todos";
+import AdminTodos from "../components/AdminTodos";
 
 function App() {
-  return (<Cats />)
+  return (
+    <>
+      <Cats />
+      <Todos />
+      <AdminTodos />
+    </>
+  );
 }
 
-export default App
+export default App;
