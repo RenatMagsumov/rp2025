bc132a4620a4eeda601b30c4d7f33db9444c8fee
RenatMagsumov <renatik@tlu.ee>
2025-10-07 19:26:32 +0300

    feat: setup local Supabase and complete Notes/Todo CRUD

--- 2025-09-25/renat/backend/routes/cats.routes.js
+++ 2025-09-25/renat/backend/routes/cats.routes.js
@@ -5,13 +5,24 @@ const {
     catsRouteMiddleware,
     catsGetRouteMiddleware,
 } = require("../middlewares/cats.middlewares");
+const { body } = require("express-validator");
 
 router.use(catsRouteMiddleware);
 
-// /cats/ Get endpoint level middleware
 router.get("/", catsGetRouteMiddleware, catsController.read);
-router.post("/", catsController.create);
+
+router.post(
+    "/",
+    body("name")
+        .isString()
+        .trim()
+        .isLength({ min: 2 })
+        .withMessage("Name must be at least 2 characters"),
+    catsController.create
+);
+
 router.put("/", catsController.update);
+
 router.delete("/", catsController.delete);
 
 module.exports = router;
