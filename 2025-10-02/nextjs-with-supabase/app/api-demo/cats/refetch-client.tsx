d089018165c7e96eeb70c397507f0a593d22121c
RenatMagsumov <renatik@tlu.ee>
2025-10-16 14:31:02 +0300

    feat(cats): implement client-side fetch for random cat fact

--- 2025-10-02/nextjs-with-supabase/app/api-demo/cats/refetch-client.tsx
+++ 2025-10-02/nextjs-with-supabase/app/api-demo/cats/refetch-client.tsx
@@ -8,9 +8,15 @@ type CatFact = { text: string; type: string; _id: string };
 export default function ClientCatFact() {
     const [fact, setFact] = useState<CatFact | null>(null);
 
+    const getNewFact = async () => {
+        const res = await fetch("https://cat-fact.herokuapp.com/facts/random");
+        const data = (await res.json()) as CatFact;
+        setFact(data);
+    };
+
     return (
         <>
-            <Button></Button>
+            <Button onClick={getNewFact}>Get new cat fact</Button>
             {fact && <Text mt="sm">Client fact: {fact.text}</Text>}
         </>
     );
